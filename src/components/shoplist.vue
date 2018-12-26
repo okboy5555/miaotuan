fba054d1f68641291cece2c646315db8101a9f9c
okboy5555 <543872824@qq.com>
2018-12-26 18:54:59 +0800

    祝自己在新的一年更上一层楼---头像

--- src/components/shoplist.vue
+++ src/components/shoplist.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    shoplist
+    <section class="shop-list">
+      <section>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+    </section>
   </div>
 </template>
--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
       <Header></Header>
-      <div>mine</div>
+      <div class="top">
+        <div class="headportrait">
+          <span>头像</span>
+        </div>
+        <div>2</div>
+        <div>3</div>
+      </div>
       <Footer></Footer>
   </div>
 </template>
@@ -18,3 +24,20 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+  .top {
+    box-sizing: border-box;
+    display: flex;
+    height: 6rem;
+    background-image: linear-gradient(90deg,#0af,#0085ff);
+    padding: 1.5rem 1.4rem;
+    color: #fff;
+    align-items: center;
+    .headportrait {
+      width: 4rem;
+      height: 4rem;
+      background: white;
+      border-radius: 50%;
+    }
+  }
+</style>
