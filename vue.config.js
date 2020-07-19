da9b8e3333f13a4515b27aff74ec41615833a265
mogong <mogong@qunhemail.com>
2020-07-19 14:58:58 +0800

    fix: 修改sass-loader配置

--- vue.config.js
+++ vue.config.js
@@ -1,41 +1,43 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
-    // 基本路径
-    baseUrl: '/',
-    // 输出文件目录
-    outputDir: 'dist',
-    // 静态文件目录
-    assetsDir: 'assets',
-    // 指定index.html生成路径
-    indexPath: 'index.html',
-    // 文件名中包含hash
-    filenameHashing: true,
-    // 运行时使用编译器的vue构建版本
-    runtimeCompiler: false,
-    // 向css相关loader传递选项
-    css: {
-        loaderOptions: {
-            css: {
-                // 在这里的选项会传递给css-loader
-            },
-            postcss: {
-                // 在这里的选项会传递给postcss-loader
-            },
-            sass: {
-                data: `@import "@/styles/imports.scss";@import "@/styles/common.scss";`,
-            }
+  // 基本路径
+  publicPath: "/",
+  // 输出文件目录
+  outputDir: "dist",
+  // 静态文件目录
+  assetsDir: "assets",
+  // 指定index.html生成路径
+  indexPath: "index.html",
+  // 文件名中包含hash
+  filenameHashing: true,
+  // 运行时使用编译器的vue构建版本
+  runtimeCompiler: false,
+  // 向css相关loader传递选项
+  css: {
+    loaderOptions: {
+      css: {
+        // 在这里的选项会传递给css-loader
+      },
+      postcss: {
+        // 在这里的选项会传递给postcss-loader
+      },
+      sass: {
+        // prependData: `@import "@/styles/imports.scss";@import "@/styles/common.scss";`
+        sassOptions: {
+          prependnData: `~@import "@/styles/imports.scss";@import "~@/styles/common.scss";`
         }
-    },
-    // webpakc配置
-    // webpack-dev-server
-    devServer: {
-        open: true, // 是否自动打开浏览器
-        host: '0.0.0.0',
-        port: '8888',
-        https: false, // 是否开启https
-        // proxy: {
-        //     // 配置代理
-        // }
-    },
-   
-}
+      }
+    }
+  },
+  // webpakc配置
+  // webpack-dev-server
+  devServer: {
+    open: true, // 是否自动打开浏览器
+    host: "0.0.0.0",
+    port: "8888",
+    https: false // 是否开启https
+    // proxy: {
+    //     // 配置代理
+    // }
+  }
+};
